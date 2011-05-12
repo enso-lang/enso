153dc6dd9da19497dfc88dbdd43a4d8f69d91074
Tijs van der Storm <storm@cwi.nl>
2011-05-13 01:00:58 +0200

    First sketches of Web models.

---
+++ src/core/web/code/test.rb
@@ -0,0 +1,10 @@
+
+
+require 'core/system/load/load'
+require 'core/schema/tools/print'
+
+#wg = Loader.load('web.grammar')
+#w = Loader.load('web.schema')
+e = Loader.load('example.web')
+
+puts e.to_s
