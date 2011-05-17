989d3f5a97b850bf0ce3d31dee320cf0ce5f1c07
Tijs van der Storm <storm@cwi.nl>
2011-05-17 06:07:03 +0200

    Unparse based on sppfs.

---
+++ src/core/grammar/code/gll/unparse.rb
@@ -0,0 +1,42 @@
+
+
+class Unparse
+
+  def self.unparse(sppf, out = $stdout)
+    Unparse.new.recurse(sppf, out)
+    return out
+  end
+
+  def recurse(sppf, out)
+    type = sppf.type 
+    sym = type.schema_class.name
+    if respond_to?(sym)
+      send(sym, type, sppf, out)
+    else
+      kids(sppf, out)
+    end
+  end
+
+  def kids(sppf, out)
+    return if sppf.kids.empty?
+    pack = sppf.kids.first
+    recurse(pack.left, out) if pack.left
+    recurse(pack.right, out)
+  end
+
+  def Lit(this, sppf, out)
+    out << sppf.value
+    out << sppf.ws
+  end
+
+  def Value(this, sppf, out)
+    out << sppf.value
+    out << sppf.ws
+  end
+
+  def Ref(this, sppf, accu, in_field)
+    out << sppf.value
+    out << sppf.ws
+  end
+
+end
