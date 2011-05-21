0143ee6c41e29d4cf100c2a66e029d1c0753ed34
Alex Loh <alexloh80@gmail.com>
2011-05-21 01:14:48 -0500

    Mis-merged equals.rb in previous version

--- src/core/diff/code/equals.rb
+++ src/core/diff/code/equals.rb
@@ -3,38 +3,80 @@ require 'core/grammar/code/layout'
 
 =begin
 
-Compares two structures for equality
+Deep equality based on object tree structure and primitive values
 
 =end
 
-class Equals
-  def initialize
-    @memo = {}
+class Equals 
+
+  def Equals.equals (o1, o2)
+    Equals.new.eq(o1, o2)
   end
 
-  def self.equals(a, b)
-    return self.new.equals(a, b)
+  def is_primitive_value(o)
+    if o.is_a?(Integer) 
+      return true
+    elsif o.is_a?(String) 
+      return true
+    elsif o.is_a?(TrueClass) 
+      return true
+    elsif o.is_a?(FalseClass)
+      return true
+    else
+      return false
+    end
   end
 
-  def equals(a, b)
-    return true if a == b
-    return false if a.nil? || b.nil? || a.schema_class.name != b.schema_class.name
-    return true if @memo[[a, b]]
-    @memo[[a, b]] = true
-    a.schema_class.fields.each do |field|
-      a_val = a[field.name]
-      b_val = b[field.name]
-      if field.type.Primitive?
-        return false if a_val != b_val
-      elsif !field.many
-          return false if !equals(a_val, b_val)
-      else
-        a_val.outer_join(b_val) do |a_item, b_item|
-          return false if !equals(a_item, b_item)
-        end
-      end
+  def eq (o1, o2)
+    # the irritating fake polymorphism function
+    if o1.is_a? ManyIndexedField and o2.is_a? ManyIndexedField
+      return eq_ManyIndexedField(o1, o2)
+    elsif o1.is_a? ManyField and o2.is_a? ManyField
+      return eq_ManyField(o1, o2)
+    elsif o1.schema_class.Primitive? and o2.schema_class.Primitive?
+      return eq_Primitive(o1, o2)
+    elsif o1.is_a? CheckedObject and o2.is_a? CheckedObject
+      return eq_Klass(o1, o2)
+    else # probably because o1 and o2 have different types
+      return false
+    end
+  end
+
+  def eq_Klass(o1, o2)
+    #verify that they are the same type
+    schema_class = o1.schema_class
+    return false unless o2.schema_class == schema_class
+
+    #iterate over fields
+    schema_class.fields.each do |f|
+      next unless f.traversal
+      return false unless eq(o1[f.name], o2[f.name])
+    end
+    
+    return true
+  end
+
+  def eq_Primitive(o1, o2)
+    o1 == o2
+  end
+
+  def eq_ManyField(o1, o2)
+    return false if o1.length != o2.length
+    for i in 0..o1.length-1
+      return false unless eq(o1[i], o2[i])
     end
     return true
   end
->>>>>>> c03fd89c3b9162bfc80632b56717e81ae02603fc
+
+  def eq_ManyIndexedField(o1, o2)
+    puts "in index"
+    keys = o1.keys
+    return false unless o2.keys == keys
+    #go thru key set to make sure everything is equal
+    keys.each do |k|
+      return false unless eq(o1[k], o2[k])
+    end
+    return true
+  end
+
 end
