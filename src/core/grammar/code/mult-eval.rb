0eeebbb3b657b378c03c4b4179ff57afad59818e
Tijs van der Storm <storm@cwi.nl>
2011-12-05 01:31:55 +0100

    Notes and comments

--- src/core/grammar/code/mult-eval.rb
+++ src/core/grammar/code/mult-eval.rb
@@ -26,9 +26,11 @@ end
 # to be the *argument* of the field that might be optional, many etc.).
 # In contrast, the type of a field is always derived from symbols below the
 # argument of a field (including the argument symbol itself).
+# Example: supers in schema.schema; if we start at the field arguments, supers
+# gets multiplicity + but is optional because it derives through an optional.
 class FieldMultEval < MultEval
 
-  # This is the reason for having the super class MultEval
+  # This is the reason for having the separate super class MultEval
   class Contrib < MultEval
     def Value(this, _); ONE end
     def Ref(this, _); ONE end
@@ -82,62 +84,4 @@ if __FILE__ == $0 then
     end
   end
 
-  
-
-#   # Obtain the set of classes represented
-#   # by the set of Creates in the table.
-#   classes = tbl.keys.map do |cr|
-#     cr.name
-#   end.uniq
-
-#   # Union all fields referenced in tbl for
-#   # each class.
-#   fields = {}
-#   classes.each do |cl|
-#     fields[cl] ||= Set.new
-#     tbl.each do |cr, fs|
-#       if cr.name == cl then
-#         fields[cl] |= fs
-#       end
-#     end
-#   end
-
-#   # If one of the fields for a class C does
-#   # not occur in *all* Creates representing C
-#   # then the multiplicity is seeded with ZERO.
-#   # (In other words, absence in tbl means there
-#   # is a path through the grammar where the field
-#   # never occurs. If it does occur once in another path
-#   # += will make the multiplicity optional)
-
-#   result = {}
-#   classes.each do |cl|
-#     result[cl] ||= {}
-#     fields[cl].each do |f|
-#       tbl.each do |cr, fs|
-#         if cr.name == cl then
-#           if fs.include?(f)  then
-#             meval = FieldMultEval.new(f.name)
-#             m = meval.eval(cr.arg, false)
-#             if result[cl][f.name] then
-#               result[cl][f.name] += m
-#             else
-#               result[cl][f.name] = m
-#             end
-#           elsif !result[cl][f.name]
-#             result[cl][f.name] = Multiplicity::ZERO
-#           end
-#         end
-#       end
-#     end
-#   end
-
-#   tbl.each do |cr, fs|
-#     fs.each do |f|
-#       meval = FieldMultEval.new(f.name)
-#       m = meval.eval(cr.arg, false)
-#       puts "#{cr.name}.#{f.name}: #{m}"
-#     end
-#   end
-  
 end
