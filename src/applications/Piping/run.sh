e5cd75e132998c004157c4c025545ae09a29c3f7
Tijs van der Storm <storm@cwi.nl>
2012-06-06 21:05:24 +0200

    Add -I. option.

--- src/applications/Piping/run.sh
+++ src/applications/Piping/run.sh
@@ -1,9 +1,9 @@
 
 # view the piping diagram
-arch -i386 ruby core/diagram/code/stencil.rb applications/Piping/example/boiler.piping
+arch -i386 ruby -I. core/diagram/code/stencil.rb applications/Piping/example/boiler.piping
 
 # test the simulator
-ruby applications/Piping/code/test_simulator.rb
+ruby -I. applications/Piping/code/test_simulator.rb
 
 # run the visualization
-arch -i386 ruby applications/Piping/code/visualize.rb
+arch -i386 ruby -I. applications/Piping/code/visualize.rb
