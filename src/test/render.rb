2668e8866e7274645dfde16f7d7c5ea89840ff68
William Cook <wcook@cs.utexas.edu>
2012-06-09 14:39:42 -0500

    render test

---
+++ src/test/render.rb
@@ -0,0 +1,24 @@
+  require 'core/grammar/render/layout'
+  
+  point_grammar = Loader.load('point.grammar')
+  point1 = Loader.load('point1.point')
+
+#puts "----- POINT -----"
+#  DisplayFormat.print(point_grammar, point1)
+  
+  schema_grammar = Loader.load('schema.grammar')
+  grammar_grammar = Loader.load('grammar.grammar')
+  grammar_schema = Loader.load('grammar.schema')
+  schema_schema = Loader.load('schema.schema')
+
+  DisplayFormat.print(grammar_grammar, point_grammar)
+
+#puts "----- SCHEMA GRAMMAR -----"
+#  DisplayFormat.print(grammar_grammar, schema_grammar)
+#puts "----- GRAMMAR GRAMMAR -----"
+#  DisplayFormat.print(grammar_grammar, grammar_grammar)
+#puts "----- GRAMMAR SCHEMA -----"
+#  DisplayFormat.print(schema_grammar, grammar_schema)
+#puts "----- SCHEMA SCHEMA -----"
+#  DisplayFormat.print(schema_grammar, schema_schema)
+  
