1226bd3b37462ee6f54d900488d9bd0d344f1aa4
William Cook <wcook@cs.utexas.edu>
2012-07-18 02:33:03 -0500

    Small expression sample

---
+++ src/test/samples/exp/test.rb
@@ -0,0 +1,18 @@
+
+require 'test/unit'
+
+require 'core/system/load/load'
+require 'test/samples/exp/eval.rb'
+require 'core/semantics/code/dispatch'
+
+
+class ExpTest < Test::Unit::TestCase
+
+  def test_base
+    interp = Interpreter(Eval)
+
+    ex0 = Loader.load("sample.exp")
+    assert_equal(33, interp.eval(ex0))
+  end
+
+end
