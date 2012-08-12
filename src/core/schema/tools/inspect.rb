e01cc2dc9c89a1af233a9106e7f82a0bfdbed7e5
Tijs van der Storm <storm@cwi.nl>
2012-08-12 12:48:12 +0200

    Inspect utility.

---
+++ src/core/schema/tools/inspect.rb
@@ -0,0 +1,13 @@
+
+require 'core/system/load/load'
+require 'core/schema/tools/print'
+
+if __FILE__ == $0 then
+  if !ARGV[0] then
+    $stderr << "Usage: inspect.rb <model>"
+    exit!(1)
+  end
+  model = ARGV[0]
+  m = Loader.load(model)
+  Print.print(m)
+end
