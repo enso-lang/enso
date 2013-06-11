8cbdbdc4b8bfbe135464d7dd33b1a320d11574ca
alexloh <alexloh80@gmail.com>
2013-06-10 22:22:02 -0700

    Added key field (always null) to Type in schema schema

--- src/core/schema/test/test_model.rb
+++ src/core/schema/test/test_model.rb
@@ -8,6 +8,8 @@ class ModelTest < Test::Unit::TestCase
     ss = Load::load('schema.schema')
     class_class = ss.types['Class']
     assert_equal("name", class_class.key.name)
+    int_class = ss.types['int']
+    assert_equal(nil, int_class.key)
     schema_class = ss.schema_class
     assert_equal(nil, schema_class.key)
   end
