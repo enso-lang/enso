817b935c155a3f9a7351fb9027c026780b26de92
Alex Loh <alexloh@google.com>
2014-01-30 17:52:42 -0600

    Added color to debugger

--- src/core/semantics/interpreters/debug.rb
+++ src/core/semantics/interpreters/debug.rb
@@ -1,4 +1,5 @@
 require 'core/semantics/code/interpreter'
+require 'colored'
 
 module Debug
   module Debug
@@ -25,17 +26,48 @@ module Debug
  
           #print some debugging info
           sample = @file.nil? ? "-source file not available-" : (line = this._origin.start_line-1; @file[line][this._origin.start_column..(this._origin.start_line==this._origin.end_line ? this._origin.end_column : -2)])
-          vars = @watchlist.select{|v|args[v.to_sym]}.map{|v|"#{v}=#{args[v.to_sym]}"}.join(", ")
-          $stderr << "\n\nin L#{stack.size}. #{this}:\"#{sample[0..30]}\"  #{args[:op]}(#{vars})\n"
+          vars = @watchlist.select{|v|args[v.to_sym]}.map{|v|"#{v}=#{args[v.to_sym]}"}.join("\n")
+          $stderr << "\n\nin L#{stack.size}. #{this}:\"#{sample[0..30]}\"  #{args[:op]}\n#{vars}"
                #TODO: change this to a customizable debug message?
           $stderr << "--------------------------------------------\n"
           if @file.nil?
             $stderr << "source file #{begin; this._origin.path; rescue; end} not available"
           else
-            line = this._origin.start_line-1
-            $stderr << @file[[line-@viewwidth,0].max..[line-1,0].max].map{|s|"     #{s}"}.join unless line==0
-            $stderr << "   >>"+@file[line]
-            $stderr << @file[line+1..line+@viewwidth].map{|s|"     #{s}"}.join
+              src_indicator = "  >> "
+              src_indent    = "     "
+              curr_color     = :red
+              sl = this._origin.start_line-1
+              el = this._origin.end_line-1
+              sc = this._origin.start_column
+              ec = this._origin.end_column
+              $stderr << @file[[sl-@viewwidth,0].max..[sl-1,0].max].map{|s|"     #{s}"}.join unless sl==0
+              if sc == 0 and ec == 0
+                $stderr << src_indicator + @file[sl..el].join(src_indent).red
+              elsif sl == el
+                $stderr << src_indicator
+                $stderr << @file[sl][0..sc-1] if sc > 0
+                $stderr << @file[sl][sc..ec-1].red + @file[sl][ec..-1]
+              else
+                first = middle = last = ""
+                if sc == 0
+                  first = src_indicator + @file[sl].red
+                else 
+                  first = src_indicator + @file[sl][0..sc-1] + @file[sl][sc..-1].red
+                end
+                if ec == 0
+                  last = ""
+                else
+                  last = src_indent + @file[el][0..ec-1].red + @file[el][ec..-1]
+                end
+                if el - sl < 2
+                  middle = ""
+                else
+                  middle = src_indent + @file[sl+1..el-1].join(src_indent).red
+                end
+                $stderr << first + middle + last
+              end
+              filelen = @file.length
+              $stderr << @file[[el+1,filelen].min..[el+@viewwidth,filelen].min].map{|s|"     #{s}"}.join
           end
           $stderr << "\n--------------------------------------------\n"
   
@@ -52,18 +84,6 @@ module Debug
             @stoplevel=stack.size-1
           when " ", "8" #space = resume
             @stoplevel=0
-          when "/"
-            if @file
-              print "***"
-              puts @file
-              print "***"
-              Print.print this
-              puts this._origin
-              sl = this._origin.start_line-1
-              el = this._origin.end_line-1
-              puts sl, el
-            end
-            ready = false
           when "w"
             $stderr << "Currently watching: #{@watchlist.map{|sym|sym.to_s}.join(", ")}\n"
             $stderr << "Available: #{(args.keys-@watchlist).map{|sym|sym.to_s}.join(", ")}\n"
