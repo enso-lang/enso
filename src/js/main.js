57733690d8f630722ae437512732bded7b4b5d4c
William Cook <wcook@cs.utexas.edu>
2019-07-23 10:33:47 +0800

    upgrade to latest Electron

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,11 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+ win = new BrowserWindow({
+  						webPreferences: {
+    						nodeIntegration: true
+  						}
+				})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/main.html`);
