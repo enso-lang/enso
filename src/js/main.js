117205346b4302f44200fbf9a5424fad678a4ca9
William Cook <wcook@cs.utexas.edu>
2019-07-25 11:40:44 +0800

    try again on windows security

--- src/js/main.js
+++ src/js/main.js
@@ -15,9 +15,11 @@ let win;
 function createWindow() {
   // Create the browser window.
  win = new BrowserWindow({
-  						webPreferences: {
-    						nodeIntegration: true
-  						}
+          webPreferences: {
+                nodeIntegration: true,
+                webviewTag: true,
+                allowRunningInsecureContent: true
+          }
 				})
 
   // and load the index.html of the app.
